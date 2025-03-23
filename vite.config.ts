65a8b1c7493062fcb2e167fe619345d4a121f4e5
Bryce Han <brycehan@163.com>
2025-03-23 14:19:58 +0800

    build(vite): 调整代码块大小警告限制
    -将 chunkSizeWarningLimit 从1024 KB 调整到 1500 KB
    - 此修改旨在减少不必要的警告信息，提高构建过程的可读性

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1024,
+    chunkSizeWarningLimit: 1500,
     rollupOptions: {
       output: {
         manualChunks(id) {
